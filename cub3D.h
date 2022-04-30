7847c7ad04b78832d3d8f533d17590abb4e79574
Fredi-B <fredibechtoldt@outlook.com>
2022-04-30 17:58:08 +0200

    added first draft structures

--- cub3D.h
+++ cub3D.h
@@ -1,7 +1,9 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-#include "MLX42/include/MLX42/MLX42.h"
+/* ----------------------------- Includes ---------------------------------- */
+
+# include "MLX42/include/MLX42/MLX42.h"
 # include "libft/libft.h"
 # include <stdint.h>
 # include <stdio.h>
@@ -12,6 +14,36 @@
 # include <fcntl.h>
 # include <unistd.h>
 # include <limits.h>
-#include <memory.h>
+# include <memory.h>
+
+/* ----------------------------- Defines ----------------------------------- */
+
+/* ----------------------------- Structures -------------------------------- */
+
+typedef struct s_map
+{
+	char			*north;
+	char			*south;
+	char			*west;
+	char			*east;
+	unsigned int	floor_rgb;
+	unsigned int	ceiling_rgb;
+}				t_map;
+
+typedef struct s_player
+{
+	int		position;
+	char	orientation;
+}				t_player;
+
+typedef struct s_data
+{
+	t_map		map;
+	t_player	player;
+}				t_data;
+
+/*  ------------------------ Function prototypes --------------------------- */
+
+/*  ------------------------------ main.c----------------------------------- */
 
 #endif

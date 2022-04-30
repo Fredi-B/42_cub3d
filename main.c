8847a027061dc8a262b0eee5d320cdeefc3f32bc
Fredi-B <fredibechtoldt@outlook.com>
2022-04-30 17:58:36 +0200

    deleted mlx code snippet. first steps

--- main.c
+++ main.c
@@ -1,22 +1,9 @@
 #include "cub3D.h"
 
-#define WIDTH 256
-#define HEIGHT 256
-
-static mlx_image_t *g_img;
-
-int32_t	main(void)
+int	main(int argc __attribute__ ((unused)), char **argv)
 {
-	mlx_t	*mlx;
+	t_data	data;
 
-	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
-	if (!mlx)
-		exit(EXIT_FAILURE);
-    g_img = mlx_new_image(mlx, 256, 256);    // Creates a new image.
-    mlx_image_to_window(mlx, g_img, 0, 0);   // Adds an image to the render queue.
-    mlx_put_pixel(g_img, 224, 227, 0xFFFFFFFF); // Single white pixel in the middle.
-	mlx_loop(mlx);
-	mlx_delete_image(mlx, g_img); // Once the application request an exit, cleanup.
-	mlx_terminate(mlx);
-	return (EXIT_SUCCESS);
+	parsing(&data);
+	return (0);
 }

8ba7b472b5a5f1bf425ab6aa51bcf28d78961aae
Fredi-B <fredibechtoldt@outlook.com>
2022-04-30 19:41:58 +0200

    wrote ft_cmp_file_extension(). seems to work :)

--- parsing.c
+++ parsing.c
@@ -2,7 +2,7 @@
 
 int	parsing(t_data *data, char **argv)
 {
-	if (ft_strncmp_backwards(argv[1], ".cub\0", 4) != 0)
+	if (ft_cmp_file_extension(argv[1], ".cub", 4) != 0)
 		err_exit(data, "Wrong file extension", 21, 1);
 	return (0);
 }

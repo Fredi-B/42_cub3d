cc3c723707fc6818dbfede93369ff0640baf0c3e
Arne <arnchen@gmx.net>
2022-04-30 12:54:55 +0200

    added read_file draft

---
+++ read_file.c
@@ -0,0 +1,23 @@
+#include "cub3D.h"
+
+int	read_file(char *file, t_array *arr)
+{
+	int		fd;
+	char	*line;
+	char	**split;
+
+	fd = open(file, O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line || line[0] == '\n')
+			break ;
+		split = ft_split_cnt(line, ' ', &(arr->cols));
+		arr->rows++;
+//		ft_resize(&*arr);
+		free(line);
+		free_2D(split);
+	}
+	close(fd);
+	return (OK);
+}

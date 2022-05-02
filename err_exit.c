083b2d08369134a74ec8f667c81c155e58fda154
Fredi-B <fredibechtoldt@outlook.com>
2022-05-02 19:15:42 +0200

    just cosmetics

--- err_exit.c
+++ err_exit.c
@@ -3,11 +3,11 @@
 /* frees everything needed. prints out error message. exits with exit_status */
 void	err_exit(t_data *data, char *msg, int len, int exit_status)
 {
-	(void) data;
 	write(STDERR_FILENO, "Error\n", 6);
 	write(STDERR_FILENO, msg, len);
 	if (len != 0)
 		write(STDERR_FILENO, "\n", 1);
+	(void) data;
 	// free_data(data);
 	exit(exit_status);
 }

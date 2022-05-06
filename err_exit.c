c471235c2733df1465697ed73725bc2a4a6d50d2
Fredi-B <fredibechtoldt@outlook.com>
2022-05-06 20:32:43 +0200

    added first frees

--- err_exit.c
+++ err_exit.c
@@ -1,5 +1,17 @@
 #include "cub3D.h"
 
+void	free_data(t_data *data)
+{
+	free(data->north);
+	data->north = NULL;
+	free(data->south);
+	data->south = NULL;
+	free(data->west);
+	data->west = NULL;
+	free(data->east);
+	data->east = NULL;
+}
+
 /* frees everything needed. prints out error message. exits with exit_status */
 void	err_exit(t_data *data, char *msg, int len, int exit_status)
 {

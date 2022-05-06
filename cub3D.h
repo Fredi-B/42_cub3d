c09152c9e1df78f38f19af07de2ff1c5d744825f
Fredi-B <fredibechtoldt@outlook.com>
2022-05-06 20:32:07 +0200

    added debug macros

--- cub3D.h
+++ cub3D.h
@@ -24,6 +24,11 @@
 # define YELLOW		0xFFFF00FF
 # define ODR		0.01745329252 //one degree in radians
 
+/* ---------------------- Defines for debugging ---------------------------- */
+# define dsprintf(expr) printf("\n" #expr ":\n|%s|\n", expr)
+# define diprintf(expr) printf("\n" #expr ":\n|%d|\n", expr)
+# define dwrite(expr) write(2, "\n" #expr "\n", strlen(#expr) + 2)
+
 /* ----------------------------- Structures -------------------------------- */
 typedef struct s_data
 {

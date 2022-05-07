0d720a48ad1b5e48b5939b2f4d1bd92badea2692
Fredi-B <fredibechtoldt@outlook.com>
2022-05-07 21:13:17 +0200

    added init_data() to init data->map

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
 #include "cub3D.h"
 
+void	init_data(t_data *data);
+
 int	main(int argc __attribute__ ((unused)), char **argv)
 {
 	t_data	data;
 
 	if (argc != 2)
 		err_exit(&data, "Wrong number of arguments", 26, 1);
+	init_data(&data);
 	parsing(&data, argv);
 	//map_init(&data);
 	//draw_map(&data);
 	return (OK);
+}
+
+void	init_data(t_data *data)
+{
+	data->map = NULL;
 }

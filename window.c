7761019dd1dbc1ede7198a251d7a9a4c660b7b5f
Fredi-B <fredibechtoldt@outlook.com>
2022-05-24 19:37:31 +0200

    rewrote Andreas' resize_texture(). seems to work

--- window.c
+++ window.c
@@ -59,20 +59,108 @@ void	pixel_put(t_data *arr, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
+void	*my_new_image(void *mlx, int size, t_image *copy)
+{
+	copy->img = mlx_new_image(mlx, size, size);
+	if (copy->img == NULL)
+		return (NULL);
+	copy->addr = mlx_get_data_addr(copy->img, &copy->bits_per_pixel, &copy->line_length, &copy->endian);
+	copy->width = size;
+	copy->height = size;
+	return (copy->img);
+}
+
+int	get_pixel_color(t_image *original, int x, int y)
+{
+	int	color;
+
+// protecten mit is_inside_image_limits()
+	color = BLACK;
+	color = *(unsigned int *)(original->addr + (unsigned int)(int)y * original->line_length + x * (original->bits_per_pixel / 8));
+	return (color);
+}
+
+int	get_color_from_texture(t_image *original, int x, int y, int size)
+{
+	int	color;
+	float	scale_x;
+	float	scale_y;
+	float	tx;
+	float	ty;
+
+	scale_x = original->width / (float)size;
+	scale_y = original->height / (float)size;
+	tx = x * scale_x;
+	ty = y * scale_y;
+	color = get_pixel_color(original, tx, ty);
+	return (color);
+}
+
+// same as pixel_put() but with t_image * instead of t_data *
+void	my_pixel_put(t_image *img, int x, int y, int color)
+{
+	char	*dst;
+	
+// protecten mit is_inside_image_limits()
+	dst = img->addr + (y * img->line_length + x * \
+							(img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	resize_texture(void *mlx, t_image *original, t_image *copy, int size)
+{
+	int	y;
+	int	x;
+	int	color;
+
+	copy->img = my_new_image(mlx, size, copy);
+	if (copy->img == NULL)
+		return ; //protecten
+	y = 0;
+	while (y < copy->height)
+	{
+		x = 0;
+		while (x < copy->width)
+		{
+			color = get_color_from_texture(original, x, y, size);
+			my_pixel_put(copy, x, y, color);
+			x++;
+		}
+		y++;
+	}
+	
+}
+
 void	map_to_image(t_data *arr)
 {
 	t_line			line;
+	t_image			*deer;
+	t_image			*copy;
+	char	*path = "deer.xpm";
 
 	mlx_clear_window(arr->mlx, arr->mlx_window);
-	arr->img = mlx_new_image(arr->mlx, arr->width, arr->height);
-	arr->addr = mlx_get_data_addr(arr->img, &arr->bits_per_pixel, \
-								&arr->size_line, &arr->endian);
+	// arr->img = mlx_new_image(arr->mlx, arr->width, arr->height);
+	// arr->addr = mlx_get_data_addr(arr->img, &arr->bits_per_pixel, \
+	// 							&arr->size_line, &arr->endian);
 	// init_line(&line);
 	// get_map(arr, &line);
-	//get_player(arr, &line);
-	//get_rays(arr, &line);
-	mlx_put_image_to_window(arr->mlx, arr->mlx_window, \
-							arr->img, 0, 0);
+	// get_player(arr, &line);
+	// get_rays(arr, &line);
+
+	deer = malloc(sizeof(t_image));
+	copy = malloc(sizeof(t_image));
+	deer->img = mlx_xpm_file_to_image(arr->mlx, path, &deer->width, &deer->height);
+	if (deer->img == NULL)
+		return ; //protecten
+	deer->addr = mlx_get_data_addr(deer->img, &deer->bits_per_pixel, &deer->line_length, &deer->endian);
+	if (deer->addr == NULL)
+		return ; //protecten
+	resize_texture(arr->mlx, deer, copy, 100);
+
+	mlx_put_image_to_window(arr->mlx, arr->mlx_window, deer->img, 500, 500);
+	mlx_put_image_to_window(arr->mlx, arr->mlx_window, copy->img, 0, 0);
+	// mlx_put_image_to_window(arr->mlx, arr->mlx_window, \
+	// 						arr->img, 0, 0);
 	return ;
 }
 
@@ -88,6 +176,6 @@ void	draw_map(t_data *arr)
 											arr->height, "cub3D");
 	
 	map_to_image(arr);
-	// mlx_key_hook(arr->mlx_window, hook, arr);
+	mlx_key_hook(arr->mlx_window, hook, arr);
 	mlx_loop(arr->mlx);
 }

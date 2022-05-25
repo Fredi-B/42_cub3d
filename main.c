2ffc910059d73e76b8ae967a60b22a13e4bd8864
Fredi-B <fredibechtoldt@outlook.com>
2022-05-25 19:07:04 +0200

    initialized data. Caused recent segfault

--- main.c
+++ main.c
@@ -17,5 +17,31 @@ int	main(int argc __attribute__ ((unused)), char **argv)
 
 static void	init_data(t_data *data)
 {
+	int	i;
+
 	data->map = NULL;
+	data->north = NULL;
+	data->south = NULL;
+	data->west = NULL;
+	data->east = NULL;
+	data->num = NULL;
+	data->mlx = NULL;
+	data->mlx_window = NULL;
+	data->img = NULL;
+	data->addr = NULL;
+	i = 0;
+	while (i < 4)
+	{
+		data->wall[i].img = NULL;
+		data->wall[i].addr = NULL;
+		i++;
+	}
+	data->xpm_file.img = NULL;
+	data->xpm_file.addr = NULL;
+	data->first_person_view.img = NULL;
+	data->first_person_view.addr = NULL;
+	data->minimap.img = NULL;
+	data-> minimap.addr = NULL;
+	data->player_in_minimap.img = NULL;
+	data->player_in_minimap.addr = NULL;
 }

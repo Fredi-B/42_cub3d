944aed5cdd541557f7c4991f8ba927876d9250f5
Fredi-B <fredibechtoldt@outlook.com>
2022-05-27 21:01:54 +0200

    finished key_hooks() and subfcts. seems to work :)

--- key_hook.c
+++ key_hook.c
@@ -1,37 +1,39 @@
 #include "cub3D.h"
 
-static int		key_pressed(int key, t_data *arr);
-static int		key_released(int key, t_data *arr);
-static void		move_y(t_data *arr, int sign);
-static void		move_x(t_data *arr, int sign);
-static void		turn(t_data *arr, int sign);
+static int	key_pressed(int key, t_data *arr);
+static int	key_released(int key, t_data *arr);
+static int	hook(t_data *arr);
 
-int	hook(int key, t_data *arr)
-{
-	if (key == ESC)
-		{
-			destroy_window(arr);
-			//exit (OK);
-		}
-	if (key == UP)
-		move_x(arr, -1);
-	if (key == DOWN)
-		move_x(arr, 1);
-	if (key == LEFT)
-		turn(arr, -1);
-	if (key == RIGHT)
-		turn(arr, 1);
-	map_to_image(arr);
-	all_images_to_window(arr);
-	return (0);
-}
+
+/* fct belongs to old approach */
+// int	hook(int key, t_data *arr)
+// {
+// 	if (key == ESC)
+// 		{
+// 			destroy_window(arr);
+// 			//exit (OK);
+// 		}
+// 	if (key == UP)
+// 		move_x(arr, -1);
+// 	if (key == DOWN)
+// 		move_x(arr, 1);
+// 	if (key == LEFT)
+// 		turn(arr, -1);
+// 	if (key == RIGHT)
+// 		turn(arr, 1);
+// 	map_to_image(arr);
+// 	all_images_to_window(arr);
+// 	return (0);
+// }
 
 void	key_hooks(t_data *arr)
 {
-	mlx_key_hook(arr->mlx_window, hook, arr); // alter Ansatz, bei dem gedrückt halten und 2 keys gleichzeitig nicht funktioniert
-	// mlx_hook(arr->mlx_window, 2, 0, &key_pressed, arr); // neuer Ansatz
-	// mlx_hook(arr->mlx_window, 3, 0, &key_released, arr);
-	// mlx_loop_hook(arr->mlx, &hook, 0); //segfaults: fct nicht in meiner mlx?
+	// mlx_key_hook(arr->mlx_window, hook, arr); // alter Ansatz, bei dem gedrückt halten und 2 keys gleichzeitig nicht funktioniert
+	// mlx_hook(arr->mlx_window, 2, 0, &hook, arr);
+	mlx_hook(arr->mlx_window, 2, 0, &key_pressed, arr); // neuer Ansatz
+	mlx_hook(arr->mlx_window, 3, 0, &key_released, arr);	
+	mlx_loop_hook(arr->mlx, &hook, arr); //segfaults: fct nicht in meiner mlx?
+
 }
 
 /* sets key_flags to on */
@@ -45,11 +47,11 @@ static int	key_pressed(int key, t_data *arr)
 	if (key == UP)
 		arr->key_flag.up = ON;
 	if (key == DOWN)
-		arr->key_flag.up = ON;
+		arr->key_flag.down = ON;
 	if (key == LEFT)
-		arr->key_flag.up = ON;
+		arr->key_flag.left = ON;
 	if (key == RIGHT)
-		arr->key_flag.up = ON;
+		arr->key_flag.right = ON;
 	return (0);
 }
 
@@ -59,72 +61,26 @@ static int	key_released(int key, t_data *arr)
 	if (key == UP)
 		arr->key_flag.up = OFF;
 	if (key == DOWN)
-		arr->key_flag.up = OFF;
+		arr->key_flag.down = OFF;
 	if (key == LEFT)
-		arr->key_flag.up = OFF;
+		arr->key_flag.left = OFF;
 	if (key == RIGHT)
-		arr->key_flag.up = OFF;
-	map_to_image(arr);			//
-	all_images_to_window(arr); // die zwei Zeilen müssten ans Ende von hook(), aber da segfaults 
+		arr->key_flag.right = OFF;
 	return (0);
 }
 
-static void	move_y(t_data *arr, int sign)
+/* update player position using key_flags */
+static int	hook(t_data *arr)
 {
-	float	y;
-	int		mx;
-	int		my;
-	int		mp;
-
-	y = arr->p_y + arr->p_dy * sign;
-	mx = (int) (arr->p_x) >> 6;
-	my = (int) (y) >> 6;
-	mp = my * arr->cols + mx;
-	// if (arr->map[mp] == '0') //hier stimmt gerade was nicht: if nie true
-		arr->p_y = y;
+	if (arr->key_flag.up == ON)
+		move_x(arr, -1);
+	if (arr->key_flag.down == ON)
+		move_x(arr, 1);
+	if (arr->key_flag.left == ON)
+		turn(arr, -1);
+	if (arr->key_flag.right == ON)
+		turn(arr, 1);
+	map_to_image(arr);
+	all_images_to_window(arr);
+	return (0);
 }
-
-static void	move_x(t_data *arr, int sign)
-{
-	float	x;
-	int		mx;
-	int		my;
-	int		mp;
-
-	x = arr->p_x + arr->p_dx * sign;
-	mx = (int) (x) >> 6;
-	my = (int) (arr->p_y) >> 6;
-	mp = my * arr->cols + mx;
-	// if (arr->map[mp] == '0') //hier stimmt gerade was nicht: if nie true
-		arr->p_x = x;
-	move_y(arr, sign);
-}
-
-static void	turn(t_data *arr, int sign)
-{
-	arr->p_a += 2 * ODR * sign;
-	if (arr->p_a >= 2 * M_PI)
-		arr->p_a -= 2 * M_PI;
-	if (arr->p_a < 0)
-			arr->p_a += 2 * M_PI;
-	arr->p_dx = cos(arr->p_a);
-	arr->p_dy = sin(arr->p_a);
-}
-
-/* supposed to update player position using key_flags */
-// int	hook(t_data *arr)
-// {
-// 	// dwrite(test);
-// 	// diprintf(arr->key_flag.up);
-// 	if (arr->key_flag.up == ON)
-// 		move_x(arr, -1);
-// 	if (arr->key_flag.down == ON)
-// 		move_x(arr, 1);
-// 	if (arr->key_flag.left == ON)
-// 		turn(arr, -1);
-// 	if (arr->key_flag.right == ON)
-// 		turn(arr, 1);
-// 	map_to_image(arr);
-// 	all_images_to_window(arr);
-// 	return (0);
-// }

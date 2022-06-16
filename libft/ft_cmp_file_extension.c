302f0821233ac34b1fa5fd0b92ff580b258e8ce5
Fredi-B <fredibechtoldt@outlook.com>
2022-06-16 16:15:03 +0200

    added protection and improved order in if statement

--- libft/ft_cmp_file_extension.c
+++ libft/ft_cmp_file_extension.c
@@ -6,7 +6,7 @@
 /*   By: fbechtol <fbechtol@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 19:40:03 by fbechtol          #+#    #+#             */
-/*   Updated: 2022/05/26 12:48:01 by fbechtol         ###   ########.fr       */
+/*   Updated: 2022/06/15 14:43:03 by fbechtol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ bool	ft_cmp_file_extension(const char *s1, const char *s2, size_t len)
 	char	**splitted_path;
 	int		i;
 
+	if (!s1 || !s2)
+		return (false);
 	splitted_path = ft_split(s1, '/');
+	if (!splitted_path)
+		return (false);
 	i = 0;
 	while (splitted_path[i + 1])
 		i++;
-	if (ft_strncmp_backwards(splitted_path[i], s2, len) != 0 \
-		|| ft_strlen(splitted_path[i]) <= len)
+	if (ft_strlen(splitted_path[i]) <= len || \
+			ft_strncmp_backwards(splitted_path[i], s2, len) != 0)
 		{
 			ft_free_two_d_arr(splitted_path);
 			return (false);

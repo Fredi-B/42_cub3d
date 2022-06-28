33afd90abffe47800474935bc37b910f89102e44
Fredi-B <fredibechtoldt@outlook.com>
2022-06-28 17:03:42 +0200

    added debug makro for floats

--- cub3D.h
+++ cub3D.h
@@ -39,6 +39,8 @@
 /* ---------------------- Defines for debugging ---------------------------- */
 # define dsprintf(expr) printf("\n" #expr ":\n|%s|\n", expr)
 # define diprintf(expr) printf("\n" #expr ":\n|%d|\n", expr)
+# define dfprintf(expr) printf("\n" #expr ":\n|%f|\n", expr)
+
 # define dwrite(expr) write(2, "\n" #expr "\n", strlen(#expr) + 2)
 
 /* ----------------------------- Structures -------------------------------- */
